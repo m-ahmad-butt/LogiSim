50d144754c5ca2eabb980df895bcfb22438ebc18
Abd Ur Rehman <naeemabdulrehman77@gmail.com>
2025-11-17 22:50:27 +0500

    feat(database): implement Singleton pattern for SQL setup class to ensure single DB instance

--- src/main/java/org/scd/dao/sqlSetup.java
+++ src/main/java/org/scd/dao/sqlSetup.java
@@ -8,30 +8,15 @@ import java.sql.Statement;
 public class sqlSetup {
 
     private static final String DB_URL = "jdbc:sqlite:database.db";
-
+    private static Connection conn;
     // Private constructor to prevent instantiation
     private sqlSetup() {}
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL);
-    }
-
-    public static void testConnection() {
-        try (Connection conn = getConnection()) {
-            if (conn != null) {
-                System.out.println("SQLite database connected successfully.");
-            }
-        } catch (SQLException e) {
-            System.out.println("Connection failed: " + e.getMessage());
+        if(conn == null){
+            conn = DriverManager.getConnection(DB_URL);
         }
+        return conn;
     }
 
-    public static void main(String[] args) throws SQLException {
-        Connection conn = getConnection();
-        String q = """
-    
-    """;
-        Statement stml = conn.createStatement();
-        stml.executeQuery(q);
-    }
 }
